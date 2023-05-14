9cad79b8e53ae6c24050ead0032e644c8f0d0529
PapaNdour <124305026+PapaNdour@users.noreply.github.com>
2023-05-14 21:50:46 +0000

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ProfilePhoto from './components/ProfilePhoto';
+import FullName from './components/FullName';
+import Address from './components/Address';
+
+const App = () => {
+  return (
+    <div>
+      <ProfilePhoto />
+      <FullName />
+      <Address />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Address.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Address = () => {
+  return (
+    <div>
+      <p>Votre Adresse</p>
+    </div>
+  );
+}
+
+export default Address;
